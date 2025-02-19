97a5951601e93e4135148f293bd0e514be474774
Sean Ciaschi <sciaschi1@gmail.com>
2025-02-19 12:20:52 -0500

    Create install-xorg-libs.sh

---
+++ install-xorg-libs.sh
@@ -0,0 +1,46 @@
+for package in $(grep -v '^#' ../lib-7.md5 | awk '{print $2}')
+do
+  packagedir=${package%.tar.?z*}
+  echo "Building $packagedir"
+
+  tar -xf $package
+  pushd $packagedir
+  docdir="--docdir=$XORG_PREFIX/share/doc/$packagedir"
+  
+  case $packagedir in
+    libXfont2-[0-9]* )
+      ./configure $XORG_CONFIG $docdir --disable-devel-docs
+    ;;
+
+    libXt-[0-9]* )
+      ./configure $XORG_CONFIG $docdir \
+                  --with-appdefaultdir=/etc/X11/app-defaults
+    ;;
+
+    libXpm-[0-9]* )
+      ./configure $XORG_CONFIG $docdir --disable-open-zfile
+    ;;
+  
+    libpciaccess* )
+      mkdir build
+      cd    build
+        meson setup --prefix=$XORG_PREFIX --buildtype=release ..
+        ninja
+        #ninja test
+        as_root ninja install
+      popd     # $packagedir
+      continue # for loop
+    ;;
+
+    * )
+      ./configure $XORG_CONFIG $docdir
+    ;;
+  esac
+
+  make
+  #make check 2>&1 | tee ../$packagedir-make_check.log
+  as_root make install
+  popd
+  rm -rf $packagedir
+  as_root /sbin/ldconfig
+done
