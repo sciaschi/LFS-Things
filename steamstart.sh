fd06c07d640a0d6e23a80c228c727c0543858c3b
Sean Ciaschi <sciaschi1@gmail.com>
2025-09-11 23:59:33 -0400

    Update steamstart.sh

--- steamstart.sh
+++ steamstart.sh
@@ -1,5 +1,8 @@
 sudo -iu steam bash -lc '
+  mkdir -p /opt/rs2/wine64/drive_c/rs2server
   cd /opt/rs2/winsteamcmd
-  curl -sSL https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip -o steamcmd.zip
-  unzip -o steamcmd.zip
+  WINEPREFIX=/opt/rs2/wine64 xvfb-run -a wine ./steamcmd.exe \
+    +login YOUR_STEAM_USERNAME \
+    +force_install_dir C:\rs2server \
+    +app_update 418480 validate +quit
 '
